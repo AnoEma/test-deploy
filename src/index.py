bfc8386925a6165f4436815850ea5ee631d2f83d
Ano Emangi Mayimona <47406426+AnoEma@users.noreply.github.com>
2024-06-27 19:17:26 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,11 +1,14 @@
 import flask
+from gevent.pywsgi import WSGIServer
+
 
 app = flask.Flask(__name__)
 
 
-@app.route('/')
+@app.route('/vamos')
 def index():
     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run()
+  http_server = WSGIServer(('191.252.201.52',8080), app)
+  http_server.serve_forever()
